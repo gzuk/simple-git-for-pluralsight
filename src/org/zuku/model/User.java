2b0458e4ef40e154f7190bcd54deda3309333b65
Grzegorz <gzuk@o2.pl>
2019-01-19 20:39:35 +0100

    Added getter and setter for name field

--- src/org/zuku/model/User.java
+++ src/org/zuku/model/User.java
@@ -3,4 +3,13 @@ package org.zuku.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
